bb64db3437f1f3e7fc414b06368f788409213644
elReclusa <howell_j@hotmail.com>
2023-02-16 14:11:12 -0600

    started working through the to do list

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,44 @@
+const { Thought } = require('../models');
+
+const resolvers = {
+  Query: {
+  Me: async (parent, args, context) => {
+    const foundUser = await User.findOne({
+        _id:context.user._id
+      });
+      return foundUser 
+    },
+
+    
+  },
+
+  Mutation: {
+    addThought: async (parent, { thoughtText, thoughtAuthor }) => {
+      return Thought.create({ thoughtText, thoughtAuthor });
+    },
+    addComment: async (parent, { thoughtId, commentText }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        {
+          $addToSet: { comments: { commentText } },
+        },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+    },
+    removeThought: async (parent, { thoughtId }) => {
+      return Thought.findOneAndDelete({ _id: thoughtId });
+    },
+    removeComment: async (parent, { thoughtId, commentId }) => {
+      return Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        { $pull: { comments: { _id: commentId } } },
+        { new: true }
+      );
+    },
+  },
+};
+
+module.exports = resolvers;
